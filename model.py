d1baff7ba6f68ea3641636cf21ad5f12f0f6d3af
Datta-IITJ <128232366+Datta-IITJ@users.noreply.github.com>
2024-04-08 14:33:43 +0530

    Add files via upload

---
+++ model.py
@@ -0,0 +1,228 @@
+class PositionalEncoding(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.dim = dim
+
+    def forward(self, noise_level):
+        count = self.dim // 2
+        step = torch.arange(count, dtype=noise_level.dtype, device=noise_level.device) / count
+        encoding = noise_level.unsqueeze(1) * torch.exp(-torch.log(torch.tensor(1e4)) * step.unsqueeze(0))
+        encoding = torch.cat([torch.sin(encoding), torch.cos(encoding)], dim=-1)
+        return encoding
+
+class FeatureWiseAffine(nn.Module):
+    def __init__(self, in_channels, out_channels, use_affine_level=False):
+        super(FeatureWiseAffine, self).__init__()
+        self.use_affine_level = use_affine_level
+        self.noise_func = nn.Sequential(nn.Linear(in_channels, out_channels*(1+self.use_affine_level)))
+
+    def forward(self, x, noise_embed):
+        noise = self.noise_func(noise_embed).view(x.shape[0], -1, 1, 1)
+        if self.use_affine_level:
+            gamma, beta = noise.chunk(2, dim=1)
+            x = (1 + gamma) * x + beta
+        else:
+            x = x + noise
+        return x
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+class Upsample(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.up = nn.Upsample(scale_factor=2, mode="nearest")
+        self.conv = nn.Conv2d(dim, dim, 3, padding=1)
+
+    def forward(self, x):
+        return self.conv(self.up(x))
+
+class Downsample(nn.Module):
+    def __init__(self, dim):
+        super().__init__()
+        self.conv = nn.Conv2d(dim, dim, 3, 2, 1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class Block(nn.Module):
+    def __init__(self, dim, dim_out, groups=32, dropout=0):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.GroupNorm(groups, dim),
+            Swish(),
+            nn.Dropout(dropout) if dropout != 0 else nn.Identity(),
+            nn.Conv2d(dim, dim_out, 3, padding=1)
+        )
+
+    def forward(self, x):
+        return self.block(x)
+
+class SelfAtt(nn.Module):
+    def __init__(self, channel_dim, num_heads, norm_groups=32):
+        super(SelfAtt,self).__init__()        
+        self.groupnorm = nn.GroupNorm(norm_groups, channel_dim)
+        self.num_heads = num_heads
+        self.qkv = nn.Conv2d(channel_dim, channel_dim * 3, 1, bias=False)
+        self.proj = nn.Conv2d(channel_dim, channel_dim, 1)
+
+    def forward(self,x):
+        b, c, h, w = x.size()
+        x = self.groupnorm(x)
+        qkv = rearrange(self.qkv(x), "b (qkv heads c) h w -> (qkv) b heads c (h w)", heads=self.num_heads, qkv=3)
+        queries, keys, values = qkv[0], qkv[1], qkv[2]
+
+        keys = F.softmax(keys, dim=-1)
+        att = torch.einsum('bhdn,bhen->bhde', keys, values)
+        out = torch.einsum('bhde,bhdn->bhen', att, queries)
+        out = rearrange(out, 'b heads c (h w) -> b (heads c) h w', heads=self.num_heads, h=h, w=w)
+
+        return self.proj(out)
+
+class ResBlock(nn.Module):
+    def __init__(self, dim, dim_out, noise_level_emb_dim=None, dropout=0, 
+                    num_heads=1, use_affine_level=False, norm_groups=32, att=True):
+        super().__init__()
+        self.noise_func = FeatureWiseAffine(noise_level_emb_dim, dim_out, use_affine_level)
+        self.block1 = Block(dim, dim_out, groups=norm_groups)
+        self.block2 = Block(dim_out, dim_out, groups=norm_groups, dropout=dropout)
+        self.res_conv = nn.Conv2d(dim, dim_out, 1) if dim != dim_out else nn.Identity()
+        self.att = att
+        self.attn = SelfAtt(dim_out, num_heads=num_heads, norm_groups=norm_groups)
+
+    def forward(self, x, time_emb):
+        y = self.block1(x)
+        y = self.noise_func(y, time_emb)
+        y = self.block2(y)
+        x = y + self.res_conv(x)
+        if self.att:
+            x = self.attn(x)
+        return x
+
+class BLVE(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(BLVE, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class BLVD(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(BLVD, self).__init__()
+        self.conv_transpose = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, padding=1)
+
+    def forward(self, x):
+        return self.conv_transpose(x)
+
+class BLVL(nn.Module):
+    def __init__(self, in_features, out_features):
+        super(BLVL, self).__init__()
+        self.fc = nn.Linear(in_features, out_features)
+
+    def forward(self, x):
+        return self.fc(x)
+
+
+class UNet(nn.Module):
+    def __init__(self, in_channel=6, out_channel=3, inner_channel=32, norm_groups=32,
+        channel_mults=[1, 2, 4, 8, 8], res_blocks=3, dropout=0, img_size=128):
+        super().__init__()
+
+        noise_level_channel = inner_channel
+        self.noise_level_mlp = nn.Sequential(
+            PositionalEncoding(inner_channel),
+            nn.Linear(inner_channel, inner_channel * 4),
+            Swish(), 
+            nn.Linear(inner_channel * 4, inner_channel)
+        )
+
+        num_mults = len(channel_mults)
+        pre_channel = inner_channel
+        feat_channels = [pre_channel]
+        now_res = img_size
+
+        downs = [nn.Conv2d(in_channel, inner_channel, kernel_size=3, padding=1)]
+        for ind in range(num_mults):
+            is_last = (ind == num_mults - 1)
+            channel_mult = inner_channel * channel_mults[ind]
+            for _ in range(0, res_blocks):
+                downs.append(ResBlock(
+                    pre_channel, channel_mult, noise_level_emb_dim=noise_level_channel, 
+                    norm_groups=norm_groups, dropout=dropout))
+                feat_channels.append(channel_mult)
+                pre_channel = channel_mult
+            if not is_last:
+                downs.append(Downsample(pre_channel))
+                feat_channels.append(pre_channel)
+                now_res = now_res//2
+        self.downs = nn.ModuleList(downs)
+
+        self.mid = nn.ModuleList([
+            ResBlock(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, 
+                            norm_groups=norm_groups, dropout=dropout),
+            ResBlock(pre_channel, pre_channel, noise_level_emb_dim=noise_level_channel, 
+                        norm_groups=norm_groups, dropout=dropout, att=False)
+        ])
+
+        # VAE branch components
+        self.BLVE = BLVE(feat_channels[-1], inner_channel)
+        self.BLVD = BLVD(inner_channel, feat_channels[-1])
+        self.BLVL = BLVL(inner_channel * now_res * now_res, inner_channel)
+
+        # Upsampling stage of U-net
+        ups = []
+        for ind in reversed(range(num_mults)):
+            is_last = (ind < 1)
+            channel_mult = inner_channel * channel_mults[ind]
+            for _ in range(0, res_blocks+1):
+                ups.append(ResBlock(
+                    pre_channel+feat_channels.pop(), channel_mult, noise_level_emb_dim=noise_level_channel, 
+                    norm_groups=norm_groups, dropout=dropout))
+                pre_channel = channel_mult
+            if not is_last:
+                ups.append(Upsample(pre_channel))
+                now_res = now_res*2
+
+        self.ups = nn.ModuleList(ups)
+
+        self.final_conv = Block(pre_channel, out_channel, groups=norm_groups)
+
+    def forward(self, x, noise_level):
+        # Embedding of time step with noise coefficient alpha
+        t = self.noise_level_mlp(noise_level)
+        
+        feats = []
+        for layer in self.downs:
+            if isinstance(layer, ResBlock):
+                x = layer(x, t)
+            else:
+                x = layer(x)
+            feats.append(x)
+
+        # VAE branch
+        BLVE_out = self.BLVE(feats[-1])
+        BLVD_out = self.BLVD(BLVE_out)
+        BLVL_in = F.relu(self.BLVL(BLVE_out.view(BLVE_out.size(0), -1)))
+        mu, logvar = BLVL_in.chunk(2, dim=1)
+
+        # KL Divergence Loss
+        KLD_loss = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+
+        # Reconstruction Loss
+        recon_loss = F.mse_loss(BLVD_out, feats[-1])
+
+        # Concatenate BLVD with U-net bottleneck output
+        #x = torch.cat((feats[-1], BLVD_out), dim=1)
+
+        for layer in self.mid:
+            x = layer(x, t)
+
+        for layer in self.ups:
+            if isinstance(layer, ResBlock):
+                x = layer(torch.cat((x, feats.pop()), dim=1), t)
+            else:
+                x = layer(x)
+
+        return self.final_conv(x), KLD_loss, recon_loss
---
+++ train.py
@@ -0,0 +1,123 @@
+import torch
+import time
+import os
+import copy
+from skimage.metrics import structural_similarity as ssim
+from utils import calculate_psnr, plot_losses
+
+class sr3:
+    def __init__(self, model, dataloader, testloader, optimizer, device, epoch, save_path, LR_size, img_size):
+        self.model = model
+        self.dataloader = dataloader
+        self.testloader = testloader
+        self.optimizer = optimizer
+        self.device = device
+        self.epoch = epoch
+        self.save_path = save_path
+        self.LR_size = LR_size
+        self.img_size = img_size
+
+    def train(self, verbose):
+        fixed_imgs1 = copy.deepcopy(next(iter(self.testloader)))
+        fixed_imgs1 = fixed_imgs1[0].to(self.device)
+        fixed_imgs = transforms.Resize(self.img_size)(transforms.Resize(self.LR_size)(fixed_imgs1))
+        train_losses = []
+        val_losses = []
+        ssim_values = []
+        psnr_values = []
+        training_starttime = time.time()
+
+        for i in range(self.epoch):
+            start_time = time.time()
+            train_loss = 0
+            KLD_loss = 0
+            kl_loss = 0
+            recon_loss = 0
+
+            for _, imgs in enumerate(self.dataloader):
+                boxes = imgs[1].to(self.device)
+                imgs = imgs[0].to(self.device)
+
+                self.optimizer.zero_grad()
+
+                x_min = boxes[0, 0, 0].item()
+                y_min = boxes[0, 0, 1].item()
+                x_max = boxes[0, 0, 2].item()
+                y_max = boxes[0, 0, 3].item()
+
+                x_min = int(x_min)
+                x_max = int(x_max)
+                y_min = int(y_min)
+                y_max = int(y_max)
+
+                if (x_max > x_min and y_max > y_min and
+                        x_max <= w and x_min >= 0 and
+                        y_max <= h and y_min >= 0):
+                    roi_imgs = imgs[:, :, x_min:x_max, y_min:y_max]
+                    roi_loss = self.model(roi_imgs)
+                    roi_loss = roi_loss.sum() / int(b * c * h * w)
+                else:
+                    roi_loss = 0
+
+                pred_SR = self.model(imgs)
+                loss = pred_SR.sum() / int(b * c * h * w)
+
+                total_loss = loss + 0.2 * roi_loss if (x_max > x_min and y_max > y_min and
+                                                        x_max <= w and x_min >= 0 and
+                                                        y_max <= h and y_min >= 0) else loss
+                total_loss += KLD_loss + recon_loss  
+
+                total_loss.backward()
+                self.optimizer.step()
+                train_loss += total_loss.item() * b
+                kl_loss += KLD_loss.item() * b
+                recon_loss += recon_loss.item() * b
+
+            self.model.eval()
+            test_imgs = next(iter(self.testloader))
+            test_imgs = test_imgs[0].to(self.device)
+            b, c, h, w = test_imgs.shape
+
+            with torch.no_grad():
+                val_loss = self.model(test_imgs)
+                val_loss = val_loss.sum() / int(b * c * h * w)
+
+                high_resolution_generated = self.model(fixed_imgs1).detach().cpu().numpy()
+                original_img = fixed_imgs1[0].permute(1, 2, 0).cpu().numpy()
+                generated_img = high_resolution_generated.squeeze().transpose(1, 2, 0)
+                ssim_value = ssim(original_img, generated_img, multichannel=True)
+                psnr_value = calculate_psnr(original_img, generated_img)
+                ssim_values.append(ssim_value)
+                psnr_values.append(psnr_value)
+
+                if verbose:
+                    print(f'Epoch: {i + 1} / SSIM:{ssim_value:.3f} / PSNR:{psnr_value:.3f}')
+
+            self.model.train()
+
+            train_loss = train_loss / len(self.dataloader)
+            kl_loss = kl_loss / len(self.dataloader)
+            recon_loss = recon_loss / len(self.dataloader)
+            train_losses.append(train_loss)
+            val_losses.append(val_loss.item())
+
+            os.makedirs("/home/dattatreyo/sr3_try/photo_output/originalnew", exist_ok=True)
+            os.makedirs("/home/dattatreyo/sr3_try/photo_output/lownew", exist_ok=True)
+            os.makedirs("/home/dattatreyo/sr3_try/photo_output/highnew", exist_ok=True)
+            os.makedirs("/home/dattatreyo/sr3_try/loss_plots", exist_ok=True)
+
+            self.save(self.save_path)
+            plot_losses(train_losses, val_losses, i + 1)
+            end_time = time.time()
+            execution_time_minutes = (end_time - start_time) / 60
+            if verbose:
+                print("Execution Time: ", format(round(execution_time_minutes, 2)), "minutes")
+
+        if verbose:
+            print("\n")
+        plot_losses(train_losses, val_losses, self.epoch)
+        training_endtime = time.time()
+        training_execution_time = training_endtime - training_starttime
+        training_execution_time_minutes = training_execution_time / 60
+        if verbose:
+            print("Training Execution Time:", format(round(training_execution_time_minutes, 2)), "minutes")
